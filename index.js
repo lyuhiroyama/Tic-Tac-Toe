4e7c00094fa5ac46cd1accb7852b18067cba6ad2
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-08-10 11:31:12 +0900

    Initialize factory function / IIFE

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-let someArr = [[],[],[]];
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square", id="squre-${index}">${square}</div>`;
+        })
+    }
+    document.querySelector("#gameboard").textContent = boardHTML; // prolly has to be innerHTML
+})();
 
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    // Game.start();
+})
