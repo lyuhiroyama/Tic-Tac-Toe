99193528c3b24fe6fe2ee9c5589d02607ea78425
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-08-12 15:28:15 +0900

    Create gameboard

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const Gameboard = (() => {
-    let gameboard = ["", "", "", "", "", "", "", "", ""];
+    let gameboard = ["", "", "", "", "", "", "", "", ""]; // Private. Accessible via render().
     const render = () => {
         let boardHTML = "";
         gameboard.forEach((square, index) => {
             boardHTML += `<div class="square", id="squre-${index}">${square}</div>`;
         })
+        document.querySelector("#gameboard").innerHTML = boardHTML; 
     }
-    document.querySelector("#gameboard").textContent = boardHTML; // prolly has to be innerHTML
+
+    return {
+        render
+    }
+})();
+
+const createPlayer = (player, mark) => {
+    return {player, mark};
+};
+
+const Game = (() => {
+    let currentPlayerIndex;
+    let gameOver;
+    const start = () => {
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render(); // ?
+    }
+    return {
+        start
+    }
 })();
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
-    // Game.start();
+    Game.start();
 })
